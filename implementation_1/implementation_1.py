1b815d608416c933f171b1bbc95a114fe46f8f84
PressW <gullysquad02@gmail.com>
2017-04-09 23:00:25 -0700

    Problems 1-5 complete

--- implementation_1/implementation_1.py
+++ implementation_1/implementation_1.py
@@ -11,8 +11,12 @@ train_file = 'housing_train.txt'
 test_file = 'housing_test.txt'
 
 
+
+
+# ------------ Helper Funcitons ------------ #
+
 def make_matrix(datafile, dummy=True):
-    
+
     df = pd.read_csv(datafile, delim_whitespace=True, header=None, names=col_names)
     if dummy is True:
         df.insert(0, 'DUMMY', 1)
@@ -23,7 +27,7 @@ def make_matrix(datafile, dummy=True):
         X_names = []
         X_names.extend(col_names)
         X_names.remove('MEDV')
-    
+
     X = df.as_matrix(columns=X_names)
     Y = df.as_matrix(columns=['MEDV'])
 
@@ -40,37 +44,27 @@ def compute_sse(X,Y,W):
     actual_predicted_diff = []
     for index, matrix in enumerate(X):
         actual_predicted_diff.append(math.pow(Y[index] - sum([W[k] * matrix[k] for k in range(len(matrix))]), 2))
-    
+
     return sum(actual_predicted_diff)
 
 
 
 def generate_random_column(limit, size):
-    rand_list = [random.uniform(0.0, float(limit)) for i in range(size)]
-    rand_col = np.array([])
-    for rand in rand_list:
-
+    return np.asarray([random.uniform(0.0, float(limit)) for i in range(size)])
 
 
 
 def generate_random_features(X, num_features):
-    print(X[0:1])
-    print(len(X))
-
     for i in range(num_features):
-        rand_list = generate_random_column((random.uniform(25.0, 500.0)), len(X))
-        new_col = [
-        print(new_col[1:10])
-        new_X = np.append(X, new_col.transpose(), 1)
+        new_col = generate_random_column((random.uniform(25.0, 500.0)), len(X))
+        new_X = np.c_[X,new_col]
         X = new_X
-
-        print(X[0:2])
-
+    return X
 
 
 
 
-
+# ------------ Show Problem Results ------------ #
 
 def problems_1_to_3():
     print("\n-------- With dummy column --------")
@@ -99,17 +93,29 @@ def problem_4():
     X_test,Y_test = make_matrix(test_file, dummy=False)
     test_sse = compute_sse(X_test, Y_test, W)
 
-    print("Testing SSE: ", test_sse, "\n")
+    print("Testing SSE: ", test_sse)
+
+
+def problem_5(num_iterations):
+    for i in range(num_iterations):
+        rand = random.randint(1, 12)
+        print("\n----- Iteration {0}: Creating {1} randomized features -----".format(i, rand))
+
+        X_train,Y_train = make_matrix(train_file)
+        X_train = generate_random_features(X_train, rand)
+        W = compute_weight(X_train, Y_train)
+        train_sse = compute_sse(X_train, Y_train, W)
+        print("Training SSE {0}: ".format(i), train_sse)
+
+        X_test,Y_test = make_matrix(train_file)
+        X_test = generate_random_features(X_test, rand)
+        test_sse = compute_sse(X_test, Y_test, W)
+        print("Testing SSE {0}: ".format(i), test_sse)
+    print("\n")
 
 
 
-#problems_1_to_3()
-#problem_4()
-
-random_list = generate_random_column(356, 15)
-X,Y = make_matrix(train_file)
-#print(random_list)
-generate_random_features(X, 3)
-
-
-
+if __name__ == "__main__":
+    #problems_1_to_3()
+    #problem_4()
+    problem_5(5)
